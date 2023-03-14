2c7825209fe805f596d559be40cdc44079287f4b
PVD-Teacher <127093027+pvd-teacher@users.noreply.github.com>
2023-03-14 16:39:37 +0530

    Update GetDataUseCase.kt

--- src/main/kotlin/domain/usecase/GetDataUseCase.kt
+++ src/main/kotlin/domain/usecase/GetDataUseCase.kt
@@ -5,6 +5,9 @@ import domain.models.RootModel
 
 class GetDataUseCase(private val dataInterface: DataInterface) {
 
+    // лучше привыкать именовать метод execute()
+    // потому что usecase содержит всегда один публичный метод 
+    // а суть ясна из названия
     fun getData(formatVariant: String): RootModel? {
         return dataInterface.getData(formatVariant)
     }
